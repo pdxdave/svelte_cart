a037a851ddc9b6379d0cbc9052d60b986d4dca93
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 08:03:05 -0800

    route pages

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,22 @@
 	import Home from './pages/Home.svelte';
 	import Products from './pages/Products.svelte';
 	import ProductTemplate from './pages/ProductTemplate.svelte';
+
+	import {Link, Router, Route} from 'svelte-routing';
 </script>
-
-<main>
-	<h1>Hello </h1>
-	
-</main>
-
+<Router>
+	<nav class="navbar">
+		<Link to="/">Home</Link>
+		<Link to="products">Products</Link>
+		<Link to="/about">About</Link>
+	</nav>
+	<Route path="/" component={Home}/>
+	<Route path="/about" component={About}/>
+	<Route path="/products" component={Products}/>
+	<Route path="/checkout" component={Checkout}/>
+	<Route path="/login" component={Login}/>
+	<Route path="/products/:id" component={ProductTemplate}/>
+</Router>
 <style>
 	
 </style>
