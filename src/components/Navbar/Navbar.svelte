91e277bc63567620fe0f698150443171a8af29b7
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 12:54:17 -0800

    close sidebar function

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -2,6 +2,13 @@
     import SmallNavbar from './SmalllNavbar.svelte';
     import BigNavbar from './BigNavbar.svelte';
     let screenWidth;
+
+    // global store
+    import globalStore from '../../stores/globalStore';
+    $: if (screenWidth > 992){
+        globalStore.toggleItem('sidebar', false)
+    }
+
 </script>
 <svelte:window bind:innerWidth={screenWidth } />
 
