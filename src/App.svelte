a79eebf82df7f62b9492b30a51634bdeb0322e20
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 07:55:40 -0800

    added pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
-	
+	// pages
+	import Login from './pages/Login.svelte';
+	import About from './pages/About.svelte';
+	import Checkout from './pages/Checkout.svelte';
+	import Home from './pages/Home.svelte';
+	import Products from './pages/Products.svelte';
+	import ProductTemplate from './pages/ProductTemplate.svelte';
 </script>
 
 <main>
---
+++ src/pages/ProductTemplate.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let id;
+    export let location;
+</script>
+
+<h1>Product Template</h1>
