adc22d69dc5c3fe12dd332c677a97eb44986f460
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 09:31:29 -0800

    add loading page

---
+++ src/components/Loading.svelte
@@ -0,0 +1,5 @@
+<div class="loading">
+    <h1>Hold on to your panties</h1>
+    <h1>loading data from someplace</h1>
+    <img src="/assets/images/loading.gif" alt="loading gif">
+</div>
--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -2,6 +2,7 @@
     export let title = '';
     import products from '../../stores/defaultProducts';
     import Product from './Product.svelte';
+    import Loading from '../Loading.svelte';
 </script>
 
 <section class="section">
@@ -9,6 +10,8 @@
     <div class="products-center">
          {#each $products as product (product.id)}
             <Product {product}/>
+        {:else}
+            <Loading />
         {/each}
     </div>
 </section>
