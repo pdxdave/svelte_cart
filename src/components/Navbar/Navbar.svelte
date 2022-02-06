ffdf789c28549858fc02538a2d05c1a4fe3f6e81
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 11:45:46 -0800

    toggle nav

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,7 +1,13 @@
 <script>
     import SmallNavbar from './SmalllNavbar.svelte';
     import BigNavbar from './BigNavbar.svelte';
+    let screenWidth;
 </script>
+<svelte:window bind:innerWidth={screenWidth } />
 
-<SmallNavbar />
-<BigNavbar />
+{#if screenWidth > 992}
+    <BigNavbar />
+{:else}
+    <SmallNavbar />
+{/if}
+
