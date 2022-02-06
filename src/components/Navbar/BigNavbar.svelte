14f95d71111af3491b544003adf5a8c71d0194dd
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-02-06 11:42:23 -0800

    links

--- src/components/Navbar/BigNavbar.svelte
+++ src/components/Navbar/BigNavbar.svelte
@@ -1,12 +1,17 @@
 <script>
     import CartButton from '../Cart/CartButton.svelte';
     import {link} from 'svelte-routing';
+    import links from '../../constants/links'
 </script>
 
 <nav class="navbar">
     <div class="nav-container">
         <div class="nav-center">
-            <p>nav links</p>
+            <ul class="nav-links">
+            {#each links as navLink}
+                <li><a href={navLink.url} use:link>{navLink.text}</a></li>
+            {/each}
+            </ul>
             <a href="/" class="nav-logo big-logo" use:link>
                 <img 
                     class="logo"
